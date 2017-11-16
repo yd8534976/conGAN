38b2ea204cdce786e35fbeb92015f35d5a26fd22
CaTheother <yd8534976@gmail.com>
2017-11-16 22:53:45 +0900

    init

--- layers.py
+++ layers.py
@@ -3,7 +3,8 @@ import tensorflow as tf
 
 def conv_bn_lrelu(inputs, filters, kernel_size=(4, 4), strides=(2, 2)):
     out_conv = tf.layers.conv2d(inputs, filters=filters, kernel_size=kernel_size,
-                                strides=strides, padding='same')
+                                strides=strides, padding='same',
+                                kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
     out_bn = tf.layers.batch_normalization(out_conv)
     out_lrelu = tf.nn.leaky_relu(out_bn)
     return out_lrelu
@@ -11,7 +12,8 @@ def conv_bn_lrelu(inputs, filters, kernel_size=(4, 4), strides=(2, 2)):
 
 def deconv_bn_relu(inputs, filters, kernel_size=(4, 4), strides=(2, 2)):
     out_conv = tf.layers.conv2d_transpose(inputs, filters=filters, kernel_size=kernel_size,
-                                          strides=strides, padding='same')
+                                          strides=strides, padding='same',
+                                          kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
     out_bn = tf.layers.batch_normalization(out_conv)
     out_relu = tf.nn.relu(out_bn)
     return out_relu
@@ -19,7 +21,8 @@ def deconv_bn_relu(inputs, filters, kernel_size=(4, 4), strides=(2, 2)):
 
 def deconv_bn_dropout_relu(inputs, filters, kernel_size=(4, 4), strides=(2, 2)):
     out_conv = tf.layers.conv2d_transpose(inputs, filters=filters, kernel_size=kernel_size,
-                                          strides=strides, padding='same')
+                                          strides=strides, padding='same',
+                                          kernel_initializer=tf.truncated_normal_initializer(stddev=0.02))
     out_bn = tf.layers.batch_normalization(out_conv)
     out_dropout = tf.layers.dropout(out_bn)
     out_relu = tf.nn.relu(out_dropout)
