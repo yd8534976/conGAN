b24f11662add03db1037f2f600d15a354134827d
CaTheother <yd8534976@gmail.com>
2017-11-17 01:43:55 +0900

    models

--- models.py
+++ models.py
@@ -123,5 +123,6 @@ def generator(inputs, name="generator"):
         d8 = deconv_bn_relu(d7, filters=3)
         # out 256x256x3
         dd = deconv_bn_relu(e1, filters=3)
-        out = tf.tanh(dd)
+        dd2 = tf.layers.dense(dd)
+        out = tf.tanh(dd2)
     return out
