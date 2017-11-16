f292e431dad6ab12587dd676ed41a5382365a289
CaTheother <yd8534976@gmail.com>
2017-11-17 03:15:57 +0900

    models

--- models.py
+++ models.py
@@ -46,7 +46,7 @@ def con_discriminator(x, y, name="discriminator"):
         h2 = conv_bn_lrelu(h1, 128)
         h3 = conv_bn_lrelu(h2, 256)
         h4 = conv_bn_lrelu(h3, 512)
-        logits = tf.layers.dense(h4, 1)
+        logits = tf.layers.dense(h4, units=1)
     return logits
 
 
