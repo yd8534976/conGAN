d5ea56039a6a5dba46115d6a26a96d196a4107b2
CaTheother <yd8534976@gmail.com>
2017-11-17 22:27:10 +0900

    debug

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ def train():
     logits_real = models.con_discriminator(x, y_, name='D2')
 
     # get loss
-    D_loss, G_loss_gan = loss.lsgan_loss(logits_fake=logits_fake, logits_real=logits_real)
-    G_loss = G_loss_gan + 100 * loss.l1_loss(y_, G_sample)
+    D_loss, G_loss_gan = loss.gan_loss(logits_fake=logits_fake, logits_real=logits_real)
+    G_loss = G_loss_gan + 1000 * loss.l1_loss(y_, G_sample)
     # get solver
     D_solver, G_solver = get_solver()
 
