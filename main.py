642150bfa547756f43f8eb96bb06c304ad4affdf
CaTheother <yd8534976@gmail.com>
2017-11-18 16:24:06 +0900

    output

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def train():
                 print("iter {}: D_loss: {}, G_loss: {}".format(it, D_loss_curr, G_loss_curr))
 
         for i in range(100):
-            samples = sess.run(G_sample, feed_dict={x: xs[i], y_: ys[i]})
+            samples = sess.run(G_sample, feed_dict={x: xs[i:i], y_: ys[i:i]})
             img = 255 * (np.array(samples + 1) / 2)
             im = Image.fromarray(np.uint8(img))
             im.save("test/epoch{}_{}.jpg".format(epoch, i))
