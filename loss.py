105970aa6b642036025474e9ac1cb461ba75cca5
CaTheother <yd8534976@gmail.com>
2017-11-19 21:57:52 +0900

    summary added

--- loss.py
+++ loss.py
@@ -15,12 +15,14 @@ def gan_loss(logits_real, logits_fake):
     - G_loss: generator loss scalar
     """
     # TODO: compute D_loss and G_loss
-    labels_ones_f = tf.ones_like(logits_fake)
-    labels_zeros_f = tf.zeros_like(logits_fake)
-    labels_ones_r = tf.ones_like(logits_real)
-    G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels_ones_f, logits=logits_fake))
-    D_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels_ones_r, logits=logits_real))
-    D_loss += tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels_zeros_f, logits=logits_fake))
+    with tf.variable_scope("G_loss"):
+        labels_ones_f = tf.ones_like(logits_fake)
+        G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels_ones_f, logits=logits_fake))
+    with tf.variable_scope("D_loss"):
+        labels_zeros_f = tf.zeros_like(logits_fake)
+        labels_ones_r = tf.ones_like(logits_real)
+        D_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels_ones_r, logits=logits_real))
+        D_loss += tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels_zeros_f, logits=logits_fake))
 
     return D_loss, G_loss
 
@@ -37,4 +39,6 @@ def lsgan_loss(logits_real, logits_fake):
 
 
 def l1_loss(real, fake):
-    return tf.reduce_mean(tf.abs(real - fake))
+    with tf.variable_scope("L1_loss"):
+        loss = tf.reduce_mean(tf.abs(real - fake))
+    return loss
